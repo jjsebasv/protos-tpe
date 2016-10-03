3ca9927c7fc84c62f9e87e314b3e52fd58442f16
Sebastian Vera <jj.sebas.v@gmail.com>
2016-10-03 19:19:59 -0300

    first commit

---
+++ src/main/java/ar/edu/itba/protos/Protocols/CustomTCP.java
@@ -0,0 +1,15 @@
+package ar.edu.itba.protos.Protocols;
+
+import java.io.IOException;
+import java.nio.channels.SelectionKey;
+
+/**
+ * Created by sebastian on 10/3/16.
+ */
+public interface CustomTCP {
+    void handleAccept(SelectionKey key)throws IOException;
+    void handleRead(SelectionKey key) throws IOException;
+    void handleWrite(SelectionKey key) throws IOException;
+}
+
+
